4859777638f286c4d715864bce241c21d2057b29
LeslieAine <aineleslie@gmail.com>
2023-06-07 18:13:28 +0300

    Add public recipes edit

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable, :confirmable
 
   validates :name, presence: true
+
+  
+  has_many :recipes
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :home, only: [:index]
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  # root 'home#index'
-  # Defines the root path route ("/")
-  # root "articles#index"
-  resources :users
-  resources :recipes, only: %i[index show destroy]
   root 'home#index', as: :authenticated_root
+
+  resources :users do
+    resources :foods, only: [:index, :new, :create] 
+  end
+  resources :recipes, only: [:index, :new, :create, :show, :destroy] do
+    resources :shopping_lists, only: [:index] 
+    resources :recipe_foods, only: [:new, :create, :destroy, :edit, :update]
+  end
+  get '/public_recipes', to: 'recipes#public_recipe'
 end
