6600c18b8e03c4c7d4b94b04604a280f602dcd5e
Frempong <samankwaa.frempong@gmail.com>
2023-06-07 13:52:55 +0000

    Fix linter errors

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,30 +1,28 @@
 class FoodsController < ApplicationController
-    def index
+  def index; end
+
+  def new
+    @food = Food.new
+  end
+
+  def create
+    @food = Food.new(food_params)
+    if @food.save
+      redirect_to foods_path(current_user)
+    else
+      render :new, locals: { food: @food }
     end
+  end
 
-    def new
-        @food = Food.new
-    end
+  def destroy
+    @food = Food.find(params[:id])
+    @food.destroy
+    redirect_to foods_path
+  end
 
-    def create
-        @food = Food.new(food_params)
-        if @food.save
-            redirect_to foods_path(current_user)
-        else
-            render :new, locals: { food: @food }
-        end
+  private
 
-    end
-
-    def destroy
-        @food = Food.find(params[:id])
-        @food.destroy
-        redirect_to foods_path
-    end
-
-    private
-
-    def food_params
-        params.require(:food).permit(:name, :measurement_unit, :price, :quantity).merge(user_id: current_user.id)
-    end
+  def food_params
+    params.require(:food).permit(:name, :measurement_unit, :price, :quantity).merge(user_id: current_user.id)
+  end
 end
