4fe8707b7b807bb9b98f22b3d4fd481dcde7347b
Frempong <samankwaa.frempong@gmail.com>
2023-06-07 13:43:01 +0000

    Add new, create and destroy actions to foods controller

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,4 +1,30 @@
 class FoodsController < ApplicationController
     def index
     end
+
+    def new
+        @food = Food.new
+    end
+
+    def create
+        @food = Food.new(food_params)
+        if @food.save
+            redirect_to foods_path(current_user)
+        else
+            render :new, locals: { food: @food }
+        end
+
+    end
+
+    def destroy
+        @food = Food.find(params[:id])
+        @food.destroy
+        redirect_to foods_path
+    end
+
+    private
+
+    def food_params
+        params.require(:food).permit(:name, :measurement_unit, :price, :quantity).merge(user_id: current_user.id)
+    end
 end
